adffd17760d3070f84a1674bba42c5af0302ff38
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-07 10:51:43 +1000

    Add props-types validator

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function Button({
 	children,
 	primary,
@@ -9,6 +11,23 @@ function Button({
 	rounded,
 }) {
 	return <button>{children}</button>
+}
+
+Button.propTypes = {
+	checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+		const count =
+			Number(!!primary) +
+			Number(!!secondary) +
+			Number(!!success) +
+			Number(!!danger) +
+			Number(!!warning)
+
+		if (count > 1) {
+			throw new Error(
+				'Only one of primary, secondary, success, danger, or warning can be true'
+			)
+		}
+	},
 }
 
 export default Button
