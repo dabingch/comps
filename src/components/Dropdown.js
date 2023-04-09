a9929f6936193b3c1768f32f30c2c72a1d5e7cbb
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-09 17:01:44 +1000

    Add Dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const Dropdown = ({ options, value, onChange }) => {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const handleClick = () => {
+		setIsOpen(!isOpen) // ! Not the correct way if the user clicks hundreds of times quickly
+	}
+
+	const handleOptionClick = (option) => {
+		setIsOpen(false)
+		onChange(option)
+	}
+
+	const renderedOptions = options.map((option) => {
+		return (
+			<div onClick={() => handleOptionClick(option)} key={option.value}>
+				{option.label}
+			</div>
+		)
+	})
+
+	return (
+		<div>
+			<div onClick={handleClick}>{value?.label || 'Select...'}</div>
+			{isOpen && <div>{renderedOptions}</div>}
+		</div>
+	)
+}
+
+export default Dropdown
