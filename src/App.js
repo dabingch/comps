98759d9a9ed8367a20454400abcaa2fd08afa473
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-10 12:19:16 +1000

    Add Link and Route component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
-function App() {}
+import Link from './components/Link'
+import Route from './components/Route'
+import AccordionPage from './pages/AccordionPage'
+import DropdownPage from './pages/DropdownPage'
+
+function App() {
+	return (
+		<div>
+			<Link to='/accordion'>Accordion</Link>
+			<Link to='/dropdown'>Dropdown</Link>
+
+			<div>
+				<Route path='/accordion'>
+					<AccordionPage />
+				</Route>
+				<Route path='/dropdown'>
+					<DropdownPage />
+				</Route>
+			</div>
+		</div>
+	)
+}
 
 export default App
