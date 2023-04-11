70530f608702439b20466daa80c7cc2817e594a4
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-11 11:53:21 +1000

    Add open and close the modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import ReactDOM from 'react-dom'
 
-function Modal() {
-	return <div>Modal</div>
+function Modal({ onClose }) {
+	// * This is a portal. It is a way to render a component outside of the
+	// * parent component. In this case, we are rendering the modal outside
+	return ReactDOM.createPortal(
+		<div>
+			<div
+				onClick={onClose}
+				className='absolute inset-0 bg-gray-300 opacity-80'
+			></div>
+			<div className='absolute inset-40 p-10 bg-white'>
+				I am a modal!!
+			</div>
+		</div>,
+		// * Target element
+		document.querySelector('.modal-container')
+	)
 }
 
 export default Modal
--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,20 @@
+import { useState } from 'react'
 import Modal from '../components/Modal'
+import Button from '../components/Button'
 
 function ModalPage() {
+	const [showModal, setShowModal] = useState(false)
+
+	const handleClick = () => setShowModal(true)
+
+	const handleClose = () => setShowModal(false)
+
 	return (
 		<div>
-			<Modal />
+			<Button onClick={handleClick} primary>
+				Open Modal
+			</Button>
+			{showModal && <Modal onClose={handleClose} />}
 		</div>
 	)
 }
