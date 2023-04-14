3bb6a092c29e4af0f98720af29466ae3d675e721
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-14 13:47:28 +1000

    Add styling for sort component

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Table from './Table'
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go'
 
 function SortableTable(props) {
 	const [sortOrder, setSortOrder] = useState(null)
@@ -7,6 +8,12 @@ function SortableTable(props) {
 	const { config, data } = props
 
 	const handleClick = (label) => {
+		if (sortBy && label !== sortBy) {
+			setSortOrder('asc')
+			setSortBy(label)
+			return
+		}
+
 		if (sortOrder === null) {
 			setSortOrder('asc')
 			setSortBy(label)
@@ -27,8 +34,14 @@ function SortableTable(props) {
 		return {
 			...column,
 			header: () => (
-				<th onClick={() => handleClick(column.label)}>
-					{column.label} IS SORTABLE
+				<th
+					className='cursor-pointer hover:bg-gray-100'
+					onClick={() => handleClick(column.label)}
+				>
+					<div className='flex items-center'>
+						{getIcons(column.label, sortBy, sortOrder)}
+						{column.label}
+					</div>
 				</th>
 			),
 		}
@@ -54,12 +67,39 @@ function SortableTable(props) {
 		})
 	}
 
-	return (
-		<div>
-			{sortOrder} - {sortBy}
-			<Table {...props} data={sortedData} config={updatedConfig} />
-		</div>
-	)
+	return <Table {...props} data={sortedData} config={updatedConfig} />
+}
+
+function getIcons(label, sortBy, sortOrder) {
+	if (label !== sortBy) {
+		return (
+			<div>
+				<GoArrowSmallUp />
+				<GoArrowSmallDown />
+			</div>
+		)
+	}
+
+	if (sortOrder === null) {
+		return (
+			<div>
+				<GoArrowSmallUp />
+				<GoArrowSmallDown />
+			</div>
+		)
+	} else if (sortOrder === 'asc') {
+		return (
+			<div>
+				<GoArrowSmallUp />
+			</div>
+		)
+	} else if (sortOrder === 'desc') {
+		return (
+			<div>
+				<GoArrowSmallDown />
+			</div>
+		)
+	}
 }
 
 export default SortableTable
