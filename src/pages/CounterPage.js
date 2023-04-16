73f4631162ce7ccfb4d70c38b1227af76039d3e2
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-16 16:31:01 +1000

    Add value to add function

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -4,9 +4,21 @@ import Panel from '../components/Panel'
 
 function CounterPage({ initialCount }) {
 	const [count, setCount] = useState(initialCount)
+	const [valueToAdd, setValueToAdd] = useState(0)
 
 	const increment = () => setCount(count + 1)
 	const decrement = () => setCount(count - 1)
+	const handleChange = (event) => {
+		const value = parseInt(event.target.value) || 0
+
+		setValueToAdd(value)
+	}
+	const handleSubmit = (event) => {
+		event.preventDefault()
+
+		setCount(count + valueToAdd)
+		setValueToAdd(0)
+	}
 
 	return (
 		<Panel className='m-3'>
@@ -20,9 +32,11 @@ function CounterPage({ initialCount }) {
 				</Button>
 			</div>
 
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label htmlFor=''>Add a lot</label>
 				<input
+					value={valueToAdd || ''}
+					onChange={handleChange}
 					type='number'
 					className='p-1 m-3 bg-gray-50 border-gray-300'
 				/>
