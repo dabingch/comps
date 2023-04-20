c0532ece8083994f292b3066d928ae0a106850bf
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-20 13:40:49 +1000

    Add comments for use-sort

--- src/hooks/use-sort.js
+++ src/hooks/use-sort.js
@@ -4,20 +4,27 @@ function useSort(data, config) {
 	const [sortOrder, setSortOrder] = useState(null)
 	const [sortBy, setSortBy] = useState(null)
 
+	// Define the column we need to sort by
 	const setSortColumn = (label) => {
+		// If we are sorting by a different column, reset the sortOrder
+		// and update the sortBy
 		if (sortBy && label !== sortBy) {
 			setSortOrder('asc')
 			setSortBy(label)
 			return
 		}
 
+		// If we don't have a sortOrder for a column, set it to 'asc'
 		if (sortOrder === null) {
 			setSortOrder('asc')
 			setSortBy(label)
 		} else if (sortOrder === 'asc') {
+			// If we are sorting by the same column, toggle the sortOrder
 			setSortOrder('desc')
 			setSortBy(label)
 		} else if (sortOrder === 'desc') {
+			// If we are sorting by the same column in descending order,
+			// reset the sortOrder and sortBy
 			setSortOrder(null)
 			setSortBy(null)
 		}
@@ -28,17 +35,21 @@ function useSort(data, config) {
 	// Find the correct sortValue and use it for sort
 	let sortedData = data
 	if (sortOrder && sortBy) {
+		// Find the correct sortValue based on the sortBy
 		const { sortValue } = config.find((column) => column.label === sortBy)
+
+		// ? Why use the spread operator?
 		sortedData = [...data].sort((a, b) => {
+			// sortValue is a function that returns the value we want to sort by
 			const valueA = sortValue(a)
 			const valueB = sortValue(b)
 
-			const reverseOrder = sortOrder === 'asc' ? 1 : -1
+			const reverseOrder = sortOrder === 'asc' ? 1 : -1 // Use number to define sort order
 
 			if (typeof valueA === 'string') {
-				return valueA.localeCompare(valueB) * reverseOrder
+				return valueA.localeCompare(valueB) * reverseOrder // String sort
 			} else {
-				return (valueA - valueB) * reverseOrder
+				return (valueA - valueB) * reverseOrder // Number sort
 			}
 		})
 	}
